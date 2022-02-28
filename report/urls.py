8963288a1c9564cf0067eb34d8d04dc1161fbb69
Eric Darchis <darchis@pobox.com>
2022-02-28 08:23:30 +0100

    add modular reports

--- report/urls.py
+++ report/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<str:module_name>/<str:report_name>/<str:report_format>/', views.report, name='report'),
+    path('<str:module_name>/<str:report_name>/<str:report_format>/<str:alternate>/', views.report, name='report'),
+]
